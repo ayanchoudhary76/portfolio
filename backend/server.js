1492b4054b5b0539417449cfbc42ac628d07d0ff
ayanchoudhary76 <ayanchoudhary76@gmail.com>
2026-03-23 19:39:36 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: "https://portfolio-al7s0tcyl-ayan-choudharys-projects.vercel.app",
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
